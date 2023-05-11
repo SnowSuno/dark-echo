ee151c8604c7dad94b5c9e471205257c37318f6d
Soonho Kwon <percy3368@gmail.com>
2023-05-11 10:09:01 +0900

    :art: Add import path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
+import * as path from "path";
+
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  resolve: {
+      alias: {
+        "~": path.resolve(__dirname, "src"),
+      },
+    },
 })
