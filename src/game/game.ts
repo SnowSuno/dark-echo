f4d820c74c9097f8c778c0fa478eb609f81e79ca
Soonho Kwon <percy3368@gmail.com>
2023-05-19 18:38:35 +0900

    :sparkles: Implement state navigation

--- src/game/game.ts
+++ src/game/game.ts
@@ -3,85 +3,76 @@ import type { Sketch } from "p5-svelte";
 import { Entity, Sound } from "~/entities";
 import { World } from "~/game/world";
 
-import footstepSound from "~/assets/foot.mp3";
-import windSound from "~/assets/wind.wav";
-
-import { Howl } from "howler";
 import type { Navigate } from "~/types";
+import { Audio } from "~/assets/audio";
 
-const DEBUG = true;
+const DEBUG = false;
 
-let world: World;
-let interval: NodeJS.Timer;
+export const game = (transition: Navigate, level: number): Sketch => {
+  let world: World;
+  let interval: NodeJS.Timer;
 
-let bgSound: Howl;
-let sound: Howl;
 
-export const game = (transition: Navigate, level: number): Sketch => (p5) => {
-  p5.preload = async () => {
-    bgSound = new Howl({
-      src: [windSound],
-      loop: true,
-    })
+  return (p5) => {
 
-    sound = new Howl({
-      src: [footstepSound],
-    })
 
+    p5.setup = () => {
+      Entity.injectP5(p5);
+
+      p5.createCanvas(p5.windowWidth, p5.windowHeight);
+      p5.background(0);
+
+      world = World.init();
+      Audio.background.play();
+      Audio.background.fade(0, 1, 5000);
+
+      addControls();
+    };
+
+    p5.windowResized = () => {
+      p5.resizeCanvas(p5.windowWidth, p5.windowHeight);
+      p5.background(0);
+    };
+
+    p5.draw = () => {
+      world.render();
+      world.next();
+
+      DEBUG && world.debug();
+
+      world.map.deaths.some(death => death.contains(world.player.position)) &&
+      die();
+    };
+
+    const addControls = () => {
+      console.log("add controls");
+      p5.touchStarted = () => {
+        if (interval) return;
+        createFootstep();
+        interval = setInterval(createFootstep, 600);
+      };
+
+      p5.touchEnded = () => {
+        clearInterval(interval);
+        interval = undefined;
+      };
+    };
+
+    const createFootstep = () => {
+      Audio.footstep.play();
+
+      world.player.createFootstep();
+      world.sounds.push(...Sound.createWave(world.player.position, world.map));
+    };
+
+    const die = () => {
+      Audio.background.fade(1, 0, 100);
+      Audio.background.stop();
+      Audio.footstep.fade(1, 0, 100);
+      Audio.footstep.stop();
+      clearInterval(interval);
+      p5.remove();
+      transition("restart");
+    };
   };
-
-  p5.setup = () => {
-    Entity.injectP5(p5);
-
-    p5.createCanvas(p5.windowWidth, p5.windowHeight);
-    p5.background(0);
-
-    world = World.init();
-    bgSound.play();
-    bgSound.fade(0, 1, 5000);
-  };
-
-  p5.windowResized = () => {
-    p5.resizeCanvas(p5.windowWidth, p5.windowHeight);
-    p5.background(0);
-  };
-
-  p5.draw = () => {
-    world.render();
-    world.next();
-
-    DEBUG && world.debug();
-
-    world.map.deaths.some(death => death.contains(world.player.position)) && die();
-  };
-
-  p5.touchStarted = () => {
-    if (interval) return;
-    createFootstep();
-    interval = setInterval(createFootstep, 600);
-  };
-
-  p5.touchEnded = () => {
-    clearInterval(interval);
-    interval = undefined;
-  };
-
-  const createFootstep = () => {
-    sound.play();
-
-    world.player.createFootstep();
-    world.sounds.push(...Sound.createWave(world.player.position, world.map));
-  };
-
-  const die = () => {
-    bgSound.stop();
-    p5.remove();
-    transition("restart");
-  }
-  // const navigateLevel = (restart: boolean) => {
-  //
-  //   if (restart) {
-  //     world = World.init();
-  //   }
-  // }
 };
---
+++ src/pages/Game/Level.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import { fade } from "svelte/transition";
+  import P5, { type Sketch } from "p5-svelte";
+
+  import type { Navigate } from "~/types";
+  import { game } from "~/game/index.js";
+  import { onDestroy, onMount } from "svelte";
+
+  export let navigate: Navigate;
+  export let level: number;
+
+  let sketch = game(navigate, level);
+  // onDestroy(() => {
+  //   delete this.sketch;
+  //   // sketch = undefined;
+  // });
+
+  //
+  // onMount(() => {
+  //   sketch =
+  // });
+
+</script>
+
+<main in:fade={{delay: 1200, duration: 1000}}>
+    <P5 {sketch}/>
+</main>
