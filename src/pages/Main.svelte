ed7f5e7443ae1a1422cc12713baccdbd140a8c0a
Soonho Kwon <percy3368@gmail.com>
2023-05-20 22:01:07 +0900

    :sparkles: Style title page

--- src/pages/Main.svelte
+++ src/pages/Main.svelte
@@ -1,8 +1,36 @@
 <script>
-    import { link } from "svelte-spa-router";
+  import { link } from "svelte-spa-router";
 </script>
 
 <div>
     <h1>DARK ECHO</h1>
     <a href="/game/1" use:link>start</a>
 </div>
+
+<style>
+    div {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        height: 100vh;
+        color: white;
+    }
+    h1 {
+        font-size: 5rem;
+        margin-bottom: 2rem;
+    }
+    a {
+        font-size: 2.2rem;
+        padding: 1rem 2rem;
+        border: 1px solid #000;
+        border-radius: 5px;
+        text-decoration: none;
+        color: white;
+        font-weight: 300;
+    }
+
+    a:hover {
+        text-decoration: underline;
+    }
+</style>
